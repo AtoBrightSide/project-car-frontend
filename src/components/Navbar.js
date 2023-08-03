28a6e42d82aa8cd378a215c4a0bfbee014ff26f0
Nahom Fufa <nahomn7@gmail.com>
2023-08-03 16:59:30 +0300

    -

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { Nav, Logo, Hamburger, Menu, MenuLink } from "../styles/Navbar.style";
+import { Nav, Logo, MenuLink } from "../styles/Navbar.style";
 
 export default function Navbar({ openInfoModal }) {
   const [isOpen, setisOpen] = useState(true);
